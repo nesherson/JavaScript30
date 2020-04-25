4b5b7c1d60594e2b69491eb08d15ef60ead11b71
unknown <nesad.omeragich27@gmail.com>
2020-04-25 23:30:10 +0200

    set up basic structure

--- CSS Variables/script.js
+++ CSS Variables/script.js
@@ -1,1 +1,8 @@
+const color = document.getElementById('js-color');
+const imgBackgroundColor = document.getElementById('js-background-color');
 
+function setColor() {
+  imgBackgroundColor.style.backgroundColor = color.value;
+}
+
+color.addEventListener('input', setColor);
