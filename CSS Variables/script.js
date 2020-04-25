48a25af3a64d525f97ff6002337af641ef0d98c2
unknown <nesad.omeragich27@gmail.com>
2020-04-26 00:28:35 +0200

    code refactoring

--- CSS Variables/script.js
+++ CSS Variables/script.js
@@ -1,23 +1,10 @@
-const colorInput = document.getElementById('js-color');
-const spacingInput = document.getElementById('js-spacing');
-const blurInput = document.getElementById('js-blur');
-const imgBackgroundColor = document.getElementById('js-background-color');
+const inputControls = document.querySelectorAll('#js-input-controls input');
 
-function handleColor() {
-  imgBackgroundColor.style.backgroundColor = colorInput.value;
-}
-
-function handleBlur() {
-  document.documentElement.style.setProperty(`--blur`, blurInput.value + 'px');
-}
-
-function handleSpacing() {
+function handleChange() {
   document.documentElement.style.setProperty(
-    `--spacing`,
-    spacingInput.value + 'px'
+    `--${this.name}`,
+    this.value + `${this.name === 'base' ? '' : 'px'}`
   );
 }
 
-colorInput.addEventListener('input', handleColor);
-blurInput.addEventListener('input', handleBlur);
-spacingInput.addEventListener('input', handleSpacing);
+inputControls.forEach((input) => input.addEventListener('input', handleChange));
